e943b74c3a7569ece10a367a15234f34d7533681
Barry <barrybalde@protonmail.com>
2025-05-21 23:37:14 +0200

    Initialisation du projet [Operation]

---
+++ main.py
@@ -0,0 +1,9 @@
+from operation.Operation import Operation
+
+
+
+if __name__ == "__main__":
+    
+    obj = Operation()
+
+    print(obj.factorial(4))
---
+++ operation/Operation.py
@@ -0,0 +1,47 @@
+
+
+
+class Operation:
+    """
+    @overview Cette classe permet de faire des operations mathématiques comme 
+    le `pgcd`, la `factorielle`.
+    """
+
+
+    def factorial(self, number:int) -> int | None:
+        """
+        @overview Méthode permettant de calculer la factorielle d'un nombre entier positif.
+
+        :param number {int} : La valeur pour laquelle on calcule la factorielle : n >= 0.
+
+        :return {int | None} : La factorielle du nombre en entrée sera retournée si c'est
+        un entier positif, sinon `None` sera retourné.
+
+        Use cage:
+            success :  
+                -> action : factorial(3) // (3 * 2 * 1)
+                -> return : 6
+            
+            error:
+                -> action : factorial(-3)
+                -> return : None
+        """
+
+        # Declaration de variables
+        result = 1
+
+
+        # Test du paramètre en entrée
+        if (number < 0) or ( not (isinstance(number, int)) ):
+            return None
+
+        for i in range(2, number + 1):
+            result = result * i
+
+        return result
+    
+            
+
+
+   
+
