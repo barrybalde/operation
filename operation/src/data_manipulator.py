ab6eed3153ad4ce3b25911826efdeb7e29d87571
Barry <barrybalde@protonmail.com>
2025-09-08 00:18:52 +0200

    Update the file [....data_manipulator] (ajout de méthodes pour récupérer les infos utilisateurs)

---
+++ operation/src/data_manipulator.py
@@ -0,0 +1,44 @@
+
+
+
+class DataManipulator():
+    """
+    @overview Une classe permettant de manipuler des objets comme des dictionnaires.
+    """
+
+
+    def __init__(self, data_user:dict):
+        """
+        @overview constructeur
+
+        :param data_user {dict} : Données utilisateurs.
+
+        Exemple : user_dict = {
+            '0001': {
+                'prenom': "Hawa",
+                'nom': "Bah",
+                "email": "barry@gmail.com",
+            },
+        }
+        """
+
+        self.get_data_user = data_user
+
+
+
+    def get_value_user(self, key_unique_username:str, key_value_username:str):
+        """
+        @overview une méthode qui permet de retourner une valeur donnée(à partir d'une clé).
+
+        :param key_unique_username {str} : Le username de l'utilisateur.
+        :param key_value_username {str} : La clé nous permettant de récupérer la valeur qu'on souhaite.
+
+        :return {str} : La valuer à partir du paramètre 'key_value_username'
+        """
+
+        for key_username, value_username  in self.get_data_user.items():
+
+            if key_unique_username == key_username:
+                return value_username[key_value_username]
+
+
---
+++ tests/main.py
@@ -0,0 +1,24 @@
+
+
+USER_DICT = {
+
+    '0001': {
+        'prenom': "Hawa",
+        'nom': "bah",
+        "email": "barry@gmail.com",
+    },
+
+    '0002': {
+        'prenom': "Sagacity",
+        'nom': "Quantic",
+        'email': "quantic@proton.me",
+    }
+}
+
+
+
+if __name__ == "__main__":
+
+    if('0004' == "0001"):
+
+    pass
