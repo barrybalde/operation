bee19da5762417c83d382fe47fb29e607b768d8e
Barry <barrybalde@protonmail.com>
2025-09-28 23:46:31 +0200

    Fichier modifié [...operation/src/data_manipulator.py] (méthode ajouter permettant de manipuler des données)

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Operation 
 
 Un dépot axé sur l'entrainement sur le langage de programmation Python. 
-
-etre sure qu'elle existe 
--- operation/src/data_manipulator.py
+++ operation/src/data_manipulator.py
@@ -97,3 +97,126 @@ class DataManipulator():
                 return True
             
         return False
+
+
+
+    def add_user(self, key_unique_username:str, user_info:dict) -> bool | None: 
+        """
+        @overview une méthode qui permet d'ajouter un nouveau utilisateur un dictionnaire.
+
+        :param key_unique_username {str} : La clé du dictionnaire
+        :param user_info{dict} : Un dictionnaire contenant les information de l'utilisateur.
+
+        Exemple : user_dict = {
+            '0001': {
+                'prenom': "Hawa",
+                'nom': "Bah",
+                "email": "barry@gmail.com",
+            },
+
+            '0002': {
+                'prenom': "Sagacity",
+                'nom': "Quantic",
+                'email': "quantic@proton.me",
+            }
+        }
+        """
+
+        #
+        if self.key_exists(self.get_data_user, key_unique_username):
+            return False
+        
+        #
+        if not isinstance(user_info, dict):
+            return False
+        
+
+        self.get_data_user[key_unique_username] = user_info 
+
+
+    
+    def get_users(self):
+        """
+        @overview une méthode qui permet de retourner tous les utilisateurs.
+
+        :return {dict} : Le dictionnaire.
+
+        Exemple : user_dict = {
+            '0001': {
+                'prenom': "Hawa",
+                'nom': "Bah",
+                "email": "barry@gmail.com",
+            },
+
+            '0002': {
+                'prenom': "Sagacity",
+                'nom': "Quantic",
+                'email': "quantic@proton.me",
+            }
+        }
+        """
+
+        return self.get_data_user
+    
+
+
+    def delete_user(self, key_unique_username:str):
+        """
+        @overview une méthode qui permet de supprimer un utilisateur du dictionnaire.
+
+        :param key_unique_username {str} : le username de L’utilisateur à supprimer.
+
+        :return {bool} : false si l'utilisateur n'existe pas
+
+        Exemple : user_dict = {
+            '0001': {
+                'prenom': "Hawa",
+                'nom': "Bah",
+                "email": "barry@gmail.com",
+            },
+
+            '0002': {
+                'prenom': "Sagacity",
+                'nom': "Quantic",
+                'email': "quantic@proton.me",
+            }
+
+        Pattern : method('0001')
+        """
+
+        if not self.key_exists(self.get_data_user, key_unique_username):
+            return False
+        
+        # Suppression de l'utilisateur
+        del self.get_data_user[key_unique_username]
+
+
+
+    def update_user_data(self, key_unique_user: str, key_data_to_update: str, new_value_to_put: str) -> bool:
+        """
+        @overview Une méthode qui permet de mettre à jour les informations d'un utilisateur.
+        
+        :param key_unique_user {str} : Le username unique de l’utilisateur.
+        :param key_data_to_update {str} : La clé représentant l'information à modifier.
+        :param new_value_to_put {str} : La nouvelle information à mettre remplaçant l'ancienne (par le biais de `key_data_to_update`).
+
+        :return {bool} : `True` si la si la modification a été bien effectuer ou `False` si l'utilisateur ou la clé n'existe pas.
+        """
+
+        if not self.key_exists(self.get_data_user, key_unique_user):
+            return False
+        
+
+        for key_username, data_username in self.get_data_user.items():
+
+            if key_username == key_unique_user:
+
+                if key_data_to_update in data_username:
+                    data_username[key_data_to_update] = new_value_to_put
+                    return True
+                
+                else: return False
+                
+        
+        return False
+    
